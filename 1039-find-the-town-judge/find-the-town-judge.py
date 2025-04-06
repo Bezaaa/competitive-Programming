b00dc975eb734cdef29557655ad6803040666e2f
Beza T <82864859+Bezaaa@users.noreply.github.com>
2025-04-06 14:39:21 +0300

    Time: 9 ms (82.50%) | Memory: 20.9 MB (43.15%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        trust_scores = [0] * (n + 1)
+
+        if len(trust) == 0:
+            return 1 if n == 1 else -1 
+
+        for a, b in trust:
+            trust_scores[a] -= 1 
+            trust_scores[b] += 1 
+
+        for i in range(1, n + 1):
+            if trust_scores[i] == n - 1:
+                return i
+
+        return -1
+
+
+            
+
+
+        
