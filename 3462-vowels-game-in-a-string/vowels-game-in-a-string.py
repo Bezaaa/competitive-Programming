7e6c4ec19ccb9492c696aa98ae58e0d98cd2c89e
Beza T <82864859+Bezaaa@users.noreply.github.com>
2025-09-12 14:29:57 +0300

    Time: 319 ms (6.82%) | Memory: 13.1 MB (43.18%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def doesAliceWin(self, s):
+        """
+        alice starts first 
+        Alice has to remove any non empty substring with odd number of vowels
+        Bob has to rempve any non empty substring with even number of vowels
+        The fisrt player who can't make a move on their turn loses the game
+        Return True if Alice wins elsse False
+
+        leetcoder
+        for alice to move optimally she can take the longest substring wth odd number of vowels
+        we start with how many vowels are present in the whole string if even we'll find the last vowel index from the last 
+        when will alice lose or Draw 
+        only if the vowels present are 2 
+        only if there is no vowel
+        """
+        vowels = ['a','e','i','o','u']
+        count_vowels = 0
+        for i in s:
+            if i.lower() in vowels:
+                count_vowels+=1
+        print(count_vowels)
+        if  count_vowels == 0:
+            return False
+        return True
+     
+        
