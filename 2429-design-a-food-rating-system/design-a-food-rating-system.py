b773e65cd5cd3080e6b1be6c8ba2352b372977d5
Beza T <82864859+Bezaaa@users.noreply.github.com>
2025-09-17 11:45:07 +0300

    Time: 220 ms (30.77%) | Memory: 49.2 MB (7.69%) - LeetSync

---
+++ 2429-design-a-food-rating-system/design-a-food-rating-system.py
@@ -0,0 +1,40 @@
+class FoodRatings(object):
+
+    def __init__(self, foods, cuisines, ratings):
+        """
+        :type foods: List[str]
+        :type cuisines: List[str]
+        :type ratings: List[int]
+        """
+        self.food_info = {}  
+        self.cuisine_heaps = {}  
+
+        for f, c, r in zip(foods, cuisines, ratings):
+            self.food_info[f] = [c, r]
+            if c not in self.cuisine_heaps:
+                self.cuisine_heaps[c] = []
+            heapq.heappush(self.cuisine_heaps[c], (-r, f))
+
+    def changeRating(self, food, newRating):
+        cuisine, _ = self.food_info[food]
+        self.food_info[food][1] = newRating
+        heapq.heappush(self.cuisine_heaps[cuisine], (-newRating, food))
+        
+    def highestRated(self, cuisine):
+        heap = self.cuisine_heaps[cuisine]
+      
+        while heap:
+            rating, food = heap[0]
+            if -rating == self.food_info[food][1]:
+                return food
+            heapq.heappop(heap)
+
+
+       
+        
+
+
+# Your FoodRatings object will be instantiated and called as such:
+# obj = FoodRatings(foods, cuisines, ratings)
+# obj.changeRating(food,newRating)
+# param_2 = obj.highestRated(cuisine)
