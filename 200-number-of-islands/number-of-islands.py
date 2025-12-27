450c31883a5d084d3c552315e6f447d0dd74586a
Beza T <82864859+Bezaaa@users.noreply.github.com>
2025-12-27 17:46:28 +0300

    Time: 255 ms (27.44%) | Memory: 28 MB (50.67%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,53 +1,32 @@
 class Solution(object):
     def numIslands(self, grid):
         """
-        Problem Idea:
-
-        - For every cell in the grid, we want to count how many **islands** exist.
-        - An island is a group of connected land cells (`1`) connected **vertically or horizontally**.
-        - Each island should be counted **once**, even if it has many connected cells.
-        - Water cells (`0`) or edges naturally bound islands.
-
-        Approach (DFS version):
-
-        1. Create a directions array for neighbors: up, down, left, right.
-        2. Iterate through each cell in the grid:
-            - If the cell is land (`1`), it is the start of a new island.
-            - Increment the island count by 1.
-            - Use DFS to **mark all connected land cells as visited** (convert to `0`) so they aren’t counted again.
-        3. DFS helper:
-            - For a given cell, check its neighbors in all directions.
-            - If a neighbor is within bounds and is land (`1`), recursively visit it.
-            - This ensures all cells of the current island are marked visited.
-        4. Continue iterating until all cells are processed.
-        5. Return the total island count.
+        :type grid: List[List[str]]
+        :rtype: int
         """
-
-    
-    
+        total_islands = 0
         if not grid:
-            return 0
-
-        rows, cols = len(grid), len(grid[0])
-        totalCount = 0
-
-        def dfs(r, c):
-            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] != '1':
-                return
-            # Mark as visited
-            grid[r][c] = '0'
-            # Visit neighbors
-            dfs(r+1, c)
-            dfs(r-1, c)
-            dfs(r, c+1)
-            dfs(r, c-1)
-
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == '1':
-                    totalCount += 1
-                    dfs(r, c)
-
-        return totalCount
+            return total_islands
         
+
+        def dfs (r , c):
+            # first mark the cell and neightbours as visited 
+            if (r < 0) or (r >= len(grid)) or (c < 0) or (c >= len(grid[0])) or (grid[r][c] == '0'):
+                return
+
+            # 2. MARK VISITED: Sink the ship.
+            grid[r][c] = '0'
+
+            # 3. RECURSE: Call myself on neighbors
+            dfs(r + 1, c) # Down
+            dfs(r - 1, c) # Up
+            dfs(r, c + 1) # Right
+            dfs(r, c - 1) #
+        for r in range(len(grid)):
+            for c in range (len(grid[0])):
+                if grid[r][c] == '1':
+                    total_islands+=1
+                dfs(r,c)
+        return total_islands
+
         
