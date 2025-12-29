fd1533744deab0f35a3aa6bb6a7727780a2a7e0e
Beza T <82864859+Bezaaa@users.noreply.github.com>
2025-12-29 14:41:43 +0300

    Time: 60 ms (20.63%) | Memory: 21.6 MB (89.98%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -1,34 +1,31 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution(object):
-    def lowestCommonAncestor(self, root, p, q):
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         """ 
-        Approach traverse left and right of ur tree 
-        there are 3 possibilities 
-        1.left has p or right has q or vice versa meaning they both return a val
-        2. left return but right returns None
-        3.right return but left returns None
-       for the first case if not return meaning the current node is where they split  
+        i go left and i go right if both return a value the current node is the ancestor
+        if one return a val then the val is the node
         """
-        if not root or p == root or q == root:
+
+        if not root or p ==root or q==root:
             return root
-        left_tree = self.lowestCommonAncestor(root.left , p , q)
-        right_tree = self.lowestCommonAncestor(root.right , p , q)
-        if left_tree and right_tree:
+     
+
+
+        left = self.lowestCommonAncestor(root.left , p , q)
+        right = self.lowestCommonAncestor(root.right , p , q)
+        if left and right:
             return root
-        if left_tree and not right_tree:
-            return left_tree
+        if left and not right:
+            return left
         else:
-            return right_tree
-      
+            return right
         
-
-       
-
-      
+        
+            
         
