a0507364fc1af6d08d337cc3db6901fefc6a2778
Beza T <82864859+Bezaaa@users.noreply.github.com>
2026-01-22 13:34:40 +0300

    Time: 175 ms (58.06%) | Memory: 34.5 MB (36.94%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        dq = deque([])
+        res = []
+        for i in range(len(nums)):
+            # pop the useless elements
+            while dq and  nums[i] > nums[dq[-1]]:
+                dq.pop()
+            dq.append(i)
+            # pop expired elements
+            while dq[0] == i - k:
+                dq.popleft()
+            if i >= k - 1:
+                res.append(nums[dq[0]])
+        return res
+        
+
+
+
+
+
+
+        
+       
+       
+
+
+        
