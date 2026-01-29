1b992d6c98652d98d6d46ac2c6512a5ae2ac6a1a
Beza T <82864859+Bezaaa@users.noreply.github.com>
2026-01-29 11:36:21 +0300

    Time: 6 ms (84.88%) | Memory: 21.6 MB (5.81%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,23 +1,40 @@
-class Solution(object):
-    def trap(self, height):
-        if not height:
-            return 0
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        """  
+        Approach is when do we calculate how much water it contains
+        the bottlneck is the shorter height 
+        meaning we use stack and as long as the next element is shorter than the top element in the stack
+        we add the index of the element to the stack
+        when we encounter a taller element we calculate the size of the stack and add it to the total count
+        finally return the tottal count
         
-        i, j = 0, len(height) - 1
-        max_left, max_right = height[i], height[j]
-        total = 0
-        
-        while i < j:
-            if max_left <= max_right:
-                i += 1
-                max_left = max(max_left, height[i])
-                total += max_left - height[i]
-            else:
-                j -= 1
-                max_right = max(max_right, height[j])
-                total += max_right - height[j]
-        
-        return total 
-
+        """ 
+        stack = []
+        res = 0
+        for i in range(len(height)):
+            # 1. While current height is a "Right Wall" (taller than top of stack)
+            while stack and height[i] > height[stack[-1]]:
+                
+                # 2. Pop the "Floor"
+                mid = stack.pop()
+                
+                # if no left wall exists, water leaks out. Break the while loop.
+                if not stack:
+                    break
+                    
+                # Identify the "Left Wall" (the new top of the stack)
+                left = stack[-1]
+                
+                
+                # height = (shorter of two walls) - floor
+                h = min(height[left], height[i]) - height[mid]
+                
+              
+                w = i - left - 1
+                
+                res += h * w
+                
           
-        
+            stack.append(i)
+        return res
+                
